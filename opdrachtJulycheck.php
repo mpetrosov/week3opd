364a2efa92259a1caa81e10611c7c704b5cc07f9
mpetrosov <34557212+mpetrosov@users.noreply.github.com>
2018-01-30 01:12:47 +0100

    first

---
+++ opdrachtJulycheck.php
@@ -0,0 +1,18 @@
+<?php
+
+function checkJuly(){
+    $month = true;
+
+    if($month == true){
+        echo "It's July, so it can get really hot.";
+    }else{
+        echo "It’s not July, so at least we’re not in the peak of the heat.";
+    }
+
+
+
+}
+
+checkJuly();
+
+?>
